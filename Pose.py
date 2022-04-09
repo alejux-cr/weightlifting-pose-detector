107bf100a50a8bb824081640773b05b05915f43b
alejux-cr <alejuxcr@gmail.com>
2022-04-09 00:40:51 -0600

    Implement find angles for hip, knee and ankle for squat

--- Pose.py
+++ Pose.py
@@ -15,13 +15,13 @@ class PoseDetector():
     self.pose = self.mp_pose.Pose(static_image_mode=static_img_mode, model_complexity=complexity, smooth_landmarks=smooth_landmarks,
                                   enable_segmentation=enable_segmentation, smooth_segmentation=smooth_segmentation,
                                   min_detection_confidence=min_detection_conf, min_tracking_confidence=min_tracking_conf)
+    self.landmarks_list = []
 
   def detect_pose(self, img, draw=True):
     img_to_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     self.results = self.pose.process(img_to_RGB)
     if self.results.pose_landmarks:
         if draw:
-            # annotated_image = img.copy()
             condition = np.stack((self.results.segmentation_mask) * 3, axis=-1) > 0.1
             bg_image = np.zeros(img_to_RGB.shape, dtype=np.uint8)
             bg_image[:] = self.BG_COLOR
@@ -32,28 +32,31 @@ class PoseDetector():
     return img
 
   def find_position(self, img, draw=True):
-    self.lmList = []
+    self.landmarks_list = []
     if self.results.pose_landmarks:
         for id, lm in enumerate(self.results.pose_landmarks.landmark):
           h, w, c = img.shape
           cx, cy = int(lm.x * w), int(lm.y * h)
-          self.lmList.append([id, cx, cy])
+          self.landmarks_list.append([id, cx, cy])
           if draw:
             cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
-    return self.lmList
+    return self.landmarks_list
 
-  def find_angle(self, img, p1, p2, p3, draw=True):
+  def find_angle(self, img, p1, p2, p3, draw=True, inverse=False):
 
     # Get the landmarks
-    x1, y1 = self.lmList[p1][1:]
-    x2, y2 = self.lmList[p2][1:]
-    x3, y3 = self.lmList[p3][1:]
+    x1, y1 = self.landmarks_list[p1][1:]
+    x2, y2 = self.landmarks_list[p2][1:]
+    x3, y3 = self.landmarks_list[p3][1:]
 
     # Calculate the Angle
-    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
-                          math.atan2(y1 - y2, x1 - x2))
+    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
     if angle < 0:
       angle += 360
+    else:
+      if angle > 180 and inverse:
+        angle = 360 - angle
+
 
     if draw:
       cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
@@ -64,7 +67,6 @@ class PoseDetector():
       cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
       cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
       cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
-      cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
-                  cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+      cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
 
     return angle
--- Weightlifter.py
+++ Weightlifter.py
@@ -20,7 +20,20 @@ while True:
   success, img = cap.read()
   if img is not None:
     img = cv2.resize(img, (width, height))
-    img = detector.detect_pose(img)
+    img = detector.detect_pose(img, False)
+    landmarks_list = detector.find_position(img, False)
+
+    if len(landmarks_list) > 0:
+      hip_angle = detector.find_angle(img, 12, 24, 26)
+      hip_percentage = np.interp(hip_angle, (210, 310), (0, 100))
+
+      knee_angle = detector.find_angle(img, 24, 26, 28, True, True)
+      knee_percentage = np.interp(knee_angle, (210, 310), (0, 100))
+
+      ankle_angle = detector.find_angle(img, 26, 28, 32)
+      anke_percentage = np.interp(hip_angle, (210, 310), (0, 100))
+      # print(angle, percentage)
+
     cv2.imshow("Weightlifting", img)
     key = cv2.waitKey(1)
     if  key == ord('q'):
